8aa3b5d80c77a9ae31eb16f703355c02ed16c667
rahmatwidadi <rahmatwidadi@users.noreply.github.com>
2017-09-24 17:02:06 +0700

    Add files via upload

---
+++ firmware_tugas_pyqt_arduino.ino
@@ -0,0 +1,161 @@
+//data serial
+int inbytes = 1;
+byte RXBuf [64];
+//int jalan = 0;
+
+// motor satu
+int enA = 10;
+int in1 = 9;
+int in2 = 8;
+
+// motor dua
+int enB = 5;
+int in3 = 7;
+int in4 = 6;
+
+void setup()
+{
+// set all the motor control pins to outputs
+Serial.begin(9600);
+pinMode(enA, OUTPUT);
+pinMode(enB, OUTPUT);
+pinMode(in1, OUTPUT);
+pinMode(in2, OUTPUT);
+pinMode(in3, OUTPUT);
+pinMode(in4, OUTPUT);
+}
+
+void stay()
+{
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+  analogWrite (enA, 0);
+  analogWrite (enB, 0);
+}
+
+void forward()
+{
+  //motor 1
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2,  LOW);
+  analogWrite (enA, 100);
+  //motor 2
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, LOW);
+  analogWrite (enB, 120);
+}
+
+void reverse()
+{
+  //motor 1
+  digitalWrite (in1,  LOW);
+  digitalWrite (in2,  HIGH);
+  analogWrite (enA, 100);
+  //motor 2
+  digitalWrite (in3, LOW);
+  digitalWrite (in4, HIGH);
+  analogWrite (enB, 140);
+}
+
+void turn_forward_left()
+{
+  //motor 1
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2,  LOW);
+  analogWrite (enA, 120);
+  //motor 2
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, LOW);
+  analogWrite (enB, 0);
+}
+
+void turn_forward_right()
+{
+  //motor 1
+  digitalWrite (in1, HIGH);
+  digitalWrite (in2,  LOW);
+  analogWrite (enA, 0);
+  //motor 2
+  digitalWrite (in3, HIGH);
+  digitalWrite (in4, LOW);
+  analogWrite (enB, 150);
+}
+
+void turn_reverse_right()
+{
+  //motor 1
+  digitalWrite (in1, LOW);
+  digitalWrite (in2,  HIGH);
+  analogWrite (enA, 0);
+  //motor 2
+  digitalWrite (in3, LOW);
+  digitalWrite (in4, HIGH);
+  analogWrite (enB, 150);
+}
+
+void turn_reverse_left()
+{
+  //motor 1
+  digitalWrite (in1,  LOW);
+  digitalWrite (in2,  HIGH);
+  analogWrite (enA, 120);
+  //motor 2
+  digitalWrite (in3, LOW);
+  digitalWrite (in4, HIGH);
+  analogWrite (enB, 0);
+}
+
+
+void loop()
+{
+  if (Serial.available()) 
+  {
+    int inbytes = Serial.readBytes(RXBuf,sizeof(RXBuf));
+    //int inbytes = Serial.readBytes(RXbuf,sizeof(RXbuf));
+    switch (inbytes) {
+      case 1:
+        stay();
+        //Serial.print("Stop");
+        break;
+      case 2:
+        forward();
+        //Serial.print("Constan Forward");
+        //delay(3000);
+        break;
+      case 3:
+        turn_forward_right();
+        delay (1000);
+        forward();
+        //Serial.print("Constan Turn Rigt");
+        break;
+      case 4:
+        turn_reverse_right();
+        delay (1000);
+        reverse();
+        break;
+      case 5:
+        reverse();
+        //Serial.print("Constan Backward");
+        break;
+      case 6:
+        turn_reverse_left();
+        delay (1000);
+        reverse ();
+        //Serial.print("constan Turn Left");
+        break;
+      case 7:
+        turn_forward_left();
+        delay (1000);
+        forward ();
+        //Serial.print("constan Turn Left");
+        break;
+      default:
+        stay();
+        //Serial.print("Stop");
+    }
+  }
+   
+}
+
---
+++ phyton_tugas_pyqt_arduino.py
@@ -0,0 +1,119 @@
+﻿import sys, os, glob
+from PyQt5 import QtCore, QtWidgets, uic
+import serial, time
+
+qtCreatorFile = "gui_tugas_pyqt_arduino.ui"
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+        def __init__(self):
+                QtWidgets.QMainWindow.__init__(self)
+                Ui_MainWindow.__init__(self)
+                self.setupUi(self)
+                self.button_serial.clicked.connect(self.OpenSerial)
+                self.button_exit.clicked.connect(self.AppExit)
+##                self.TXdata = bytearray(1)
+##                self.button_forward.clicked.connect(self.stay);
+##                self.button_reverse.clicked.connect(self.stay);
+##                self.button_turn_forward_right.clicked.connect(self.stay);
+##                self.button_turn_forward_left.clicked.connect(self.stay);
+##                self.button_turn_reverse_right.clicked.connect(self.stay);
+##                self.button_turn_reverse_left.clicked.connect(self.stay);
+                self.button_forward.pressed.connect(self.forward)
+                self.button_forward.released.connect(self.stay)
+                self.button_reverse.pressed.connect(self.reverse)
+                self.button_reverse.released.connect(self.stay)
+                self.button_turn_forward_right.pressed.connect(self.turn_forward_right)
+                self.button_turn_forward_right.released.connect(self.stay)
+                self.button_turn_forward_left.pressed.connect(self.turn_forward_left)
+                self.button_turn_forward_left.released.connect(self.stay)
+                self.button_turn_reverse_right.pressed.connect(self.turn_reverse_right)
+                self.button_turn_reverse_right.released.connect(self.stay)
+                self.button_turn_reverse_left.pressed.connect(self.turn_reverse_left)                              
+                self.button_turn_reverse_left.released.connect(self.stay)
+                self.button_stay.clicked.connect(self.stay)
+                self.button_stay.setEnabled(False)
+                self.button_forward.setEnabled(False)
+                self.button_turn_forward_right.setEnabled(False)
+                self.button_turn_reverse_right.setEnabled(False)
+                self.button_reverse.setEnabled(False)
+                self.button_turn_reverse_left.setEnabled(False)
+                self.button_turn_forward_left.setEnabled(False)
+
+        def OpenSerial(self):
+                if self.button_serial.text()=='Serial':
+                        self.ser = serial.Serial("COM4", "9600", timeout=0.1)
+                        if self.ser.isOpen():
+                         self.button_serial.setText('Close Serial')
+                         self.textEdit_LogMessage.append("Opening serial port... OK")
+                         self.button_stay.setEnabled(True)
+                         self.button_forward.setEnabled(True)
+                         self.button_turn_forward_right.setEnabled(True)
+                         self.button_turn_reverse_right.setEnabled(True)
+                         self.button_reverse.setEnabled(True)
+                         self.button_turn_reverse_left.setEnabled(True)
+                         self.button_turn_forward_left.setEnabled(True)
+                        else:
+                                self.textEdit_LogMessage.append("can not open serial port")
+                else:
+                        if self.ser.isOpen():
+                                self.ser.close()
+                                self.button_serial.setText('Serial')
+                                self.textEdit_LogMessage.append("Closing serial port... OK")
+                                self.button_stay.setEnabled(False)
+                                self.button_forward.setEnabled(False)
+                                self.button_turn_forward_right.setEnabled(False)
+                                self.button_turn_reverse_right.setEnabled(False)
+                                self.button_reverse.setEnabled(False)
+                                self.button_turn_reverse_left.setEnabled(False)
+                                self.button_turn_forward_left.setEnabled(False)
+        def stay(self):
+                self.TXdata=1
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Stop")
+        
+        def forward(self):
+                self.TXdata=2
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Constan Forward")
+                time.sleep(2)
+                
+        def turn_forward_right(self):
+                self.TXdata=3
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Turn Forward Right" )
+                time.sleep(2)
+                
+        def turn_reverse_right(self):
+                self.TXdata=4
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Turn Forward Right" )
+                time.sleep(2)
+                
+        def reverse(self):
+                self.TXdata=5
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Constan Reverse" )
+                time.sleep(2)
+                
+        def turn_reverse_left(self):
+                self.TXdata=6
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Turn Reverse Left" )
+                time.sleep(2)
+                
+        def turn_forward_left(self):
+                self.TXdata=7
+                self.ser.write(self.TXdata)
+                self.textEdit_LogMessage.append("Turn Forward Left" )
+                time.sleep(2)
+                
+        def AppExit(self):
+                self.textEdit_LogMessage.setText("Exit application")
+                sys.exit()
+        
+if __name__ == "__main__":
+        app = QtWidgets.QApplication(sys.argv)
+        window = MyApp()
+        window.show()
+        sys.exit(app.exec_())
